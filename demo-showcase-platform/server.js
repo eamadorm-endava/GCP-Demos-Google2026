16d834bb6f287591e4acc5bf855876b40ffbbd85
Emmanuel Amador <emmanuel.amador@endava.com>
2026-02-13 19:05:14 +0000

    Fix react router

--- demo-showcase-platform/server.js
+++ demo-showcase-platform/server.js
@@ -68,7 +68,7 @@ Object.entries(VERTICALS).forEach(([key, targetUrl]) => {
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Fallback for React Router
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
